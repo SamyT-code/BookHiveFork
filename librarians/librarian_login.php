eb0a544d6995433065d67cfc4668bb72694c2407
Samy Touabi <stoua026@uottawa.ca>
2023-07-22 15:57:49 -0400

    fixed small favicon issue

--- librarians/librarian_login.php
+++ librarians/librarian_login.php
@@ -38,7 +38,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Librarian Login</title>
-    <link rel="icon" type="image/x-icon" href="images/bee.png">
+    <link rel="icon" type="image/x-icon" href="../images/bee.png">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/styles.css"> 
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
